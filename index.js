b85c60cbd4bb85021a36852656a2fecd00baf0c2
Zairong <57751396+zairong@users.noreply.github.com>
2026-01-12 16:07:10 +0800

    Add initial project files including TypeScript, HTML, CSS, and configuration

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const company = {
+    name: "company",
+    address: "123 Main St, Anytown, USA",
+    phone: "666666666",
+    email: "info666@company.com",
+    website: "www.company.com",
+};
+console.log('我叫' + `${company.name}`);
+console.log('我住' + `${company.address}`);
+console.log('我電話' + `${company.phone}`);
+console.log('我郵箱' + `${company.email}`);
+console.log('我網站' + `${company.website}`);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,13 @@
+const company = {
+    name: "company",
+    address: "123 Main St, Anytown, USA",
+    phone: "666666666",
+    email: "info666@company.com",
+    website: "www.company.com",
+};
+console.log('我叫' + `${company.name}`);
+console.log('我住' + `${company.address}`);
+console.log('我電話' + `${company.phone}`);
+console.log('我郵箱' + `${company.email}`);
+console.log('我網站' + `${company.website}`);
+
