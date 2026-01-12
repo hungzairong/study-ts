1dc0c5091d101459b769de55531747908037f7b4
Zairong <57751396+zairong@users.noreply.github.com>
2026-01-12 17:53:32 +0800

    Update TypeScript and HTML files to use ES2020 modules; refactor index.js and index.ts to demonstrate various TypeScript types including any, unknown, never, void, null, undefined, and symbol.

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const company = {
-    name: "company",
-    address: "123 Main St, Anytown, USA",
-    phone: "666666666",
-    email: "info666@company.com",
-    website: "www.company.com",
-};
-console.log('我叫' + `${company.name}`);
-console.log('我住' + `${company.address}`);
-console.log('我電話' + `${company.phone}`);
-console.log('我郵箱' + `${company.email}`);
-console.log('我網站' + `${company.website}`);
+// any 類型
+let a;
+a = 123;
+let x = a; // 任意類型可以賦值給任何類型
+console.log('x:', typeof x);
+// unknown 類型 安全的any
+let b;
+b = 'school';
+b.toUpperCase(); // 未知類型不能調用方法 as string 斷言
+console.log('b:', b);
+// never 類型 永遠不會有值的類型 , 通常用於限制函數返回值
+function neverReturn() {
+    throw new Error('never');
+}
+console.log('neverReturn:', neverReturn());
+// void 類型 沒有值的類型 , 通常用於限制函數返回值 ,返回值為空 undefined
+function voidReturn() {
+    console.log('void');
+}
+console.log('voidReturn:', voidReturn());
+// null 類型 沒有值的類型 , 通常用於限制變量賦值
+let e = null;
+console.log('e:', e);
+// undefined 類型 沒有值的類型 , 通常用於限制變量賦值
+let f = undefined;
+console.log('f:', f);
+// symbol 類型 唯一值的類型 , 通常用於限制變量賦值
+let g = Symbol('symbol');
+console.log('g:', g);
+export {};
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,13 +1,35 @@
-const company = {
-    name: "company",
-    address: "123 Main St, Anytown, USA",
-    phone: "666666666",
-    email: "info666@company.com",
-    website: "www.company.com",
-};
-console.log('我叫' + `${company.name}`);
-console.log('我住' + `${company.address}`);
-console.log('我電話' + `${company.phone}`);
-console.log('我郵箱' + `${company.email}`);
-console.log('我網站' + `${company.website}`);
+// any 類型
+let a: any 
+a = 123;
+let x : string = a; // 任意類型可以賦值給任何類型
+console.log('x:', typeof x); 
 
+// unknown 類型 安全的any
+let b: unknown 
+b = 'school'; 
+(b as string).toUpperCase(); // 未知類型不能調用方法 as string 斷言
+console.log('b:', b);
+
+// never 類型 永遠不會有值的類型 , 通常用於限制函數返回值
+function neverReturn(): never {
+    throw new Error('never');
+}
+console.log('neverReturn:', neverReturn());
+
+// void 類型 沒有值的類型 , 通常用於限制函數返回值 ,返回值為空 undefined
+function voidReturn(): void {
+    console.log('void');
+}
+console.log('voidReturn:', voidReturn());
+
+// null 類型 沒有值的類型 , 通常用於限制變量賦值
+let e: null = null;
+console.log('e:', e);
+
+// undefined 類型 沒有值的類型 , 通常用於限制變量賦值
+let f: undefined = undefined;
+console.log('f:', f);
+
+// symbol 類型 唯一值的類型 , 通常用於限制變量賦值
+let g: symbol = Symbol('symbol');
+console.log('g:', g);
